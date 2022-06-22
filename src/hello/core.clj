a9d6df145c8550d992b122e16dee2faec6ca3a0c
Marek Gruchała <Sigmundus2609@pc-3.home>
2022-06-22 08:36:15 +0200

    Adding a check for the  brackets

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -1,6 +1,25 @@
 (ns hello.core
   (:gen-class))
 
+(defn balanced? [s]
+    (->> s
+        (filter #{\[ \] \( \) \{ \}})
+
+        (reduce
+            (fn [stack item]
+                (cond
+                    (#{ \( \{ \[ } item) (conj stack item)
+
+                    (and (#{ \( \{ \[ } (last stack))
+                         (= ({ \) \(, \} \{, \] \[ } item) (last stack)))
+                    (pop stack)
+
+                    :else (conj stack item)))
+        [])
+
+        empty?)
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
